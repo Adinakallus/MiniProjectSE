fb2aa0f88e93f79914e265eec2dcb6e4e557864a
adina kallus <adina.ck.2000@gmail.com>
2021-03-14 21:44:35 +0200

    vector test not including fails and scale test

--- Tests/primitives/VectorTest.java
+++ Tests/primitives/VectorTest.java
@@ -2,6 +2,7 @@ package primitives;
 
 import org.junit.jupiter.api.Test;
 
+import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
 import static primitives.Util.isZero;
 
@@ -9,8 +10,14 @@ class VectorTest {
     Vector v1 = new Vector(1, 2, 3);
     Vector v2 = new Vector(-2, -4, -6);
 
+
+    // test Dot-Product
     @Test
     void dotProduct() {
+
+        assertTrue(!isZero(v1.dotProduct(v3)), "ERROR: dotProduct() for orthogonal vectors is not zero")
+        assertTrue(!isZero(v1.dotProduct(v2) + 28),"ERROR: dotProduct() wrong value)
+
     }
 
     @Test
@@ -38,29 +45,49 @@ class VectorTest {
 
     @Test
     void length() {
+                assertTrue(!isZero(new Vector(0, 3, 4).length() - 5), ("ERROR: length() wrong value")
+
+
     }
 
     @Test
     void lengthSquared() {
+        assertTrue (!isZero(v1.lengthSquared() - 14),"ERROR: lengthSquared() wrong value")
     }
 
     @Test
     void normalized() {
+        Vector u = v1.normalized();
+        assertNotEquals( Point3D.ZERO, u.subtract(v),"ERROR: normalizated() function does not create a new vector");
+
+
     }
 
     @Test
     void normalize() {
+        Vector v = new Vector(1, 2, 3);
+        Vector vCopy = new Vector(v.getHead());
+        Vector vCopyNormalize = vCopy.normalize();
+        assertEquals(Point3D.ZERO, vCopy.subtract(vCopyNormalize), 0.00001, "ERROR: normalize() function creates a new vector" );
+        assertEquals(1, vCopyNormalize.length(),  "ERROR: normalize() result is not a unit vector");
     }
 
     @Test
     void add() {
+
+        Point3D p1 = new Point3D(1, 2, 3);
+        assertTrue(!Point3D.ZERO.equals(p1.add(new Vector(-1, -2, -3))),"ERROR: Point + Vector does not work correctly");
+
     }
 
     @Test
     void subtract() {
+        Point3D p1 = new Point3D(1, 2, 3);
+        assertTrue(!new Vector(1, 1, 1).equals(new Point3D(2, 3, 4).subtract(p1))"ERROR: Point - Point does not work correctly");
     }
 
     @Test
     void scale() {
+
     }
 }
