4d41a1f9ce3815a4de273a0edb86b9bac9c4f36e
adina kallus <adina.ck.2000@gmail.com>
2021-06-02 19:36:26 +0300

    played around with our picture

--- Tests/elements/ReflectionRefractionTests.java
+++ Tests/elements/ReflectionRefractionTests.java
@@ -116,6 +116,53 @@ public class ReflectionRefractionTests {
         render.renderImage();
         render.writeToImage();
     }
+
+
+
+
+
+
+
+
+    /**
+     * Produce a picture of a sphere lighted by a spot light
+     */
+    @Test
+    public void allEffectsTest() {
+        Camera camera = new Camera(new Point3D(0, 0, 10000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+                .setViewPlaneSize(2500, 2500).setDistance(10000); //
+
+        scene.setAmbientLight(new AmbientLight(new Color(255, 255, 255), 0.1));
+
+        scene.geometries.add( //
+                new Sphere(450, new Point3D(-600, -477, -1100)) //
+                        .setEmission(new Color(0, 0, 100)) //
+                        .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20).setKt(0.6)),
+                new Sphere(200, new Point3D(-520, -700, -1090)) //
+                        .setEmission(new Color(100, 20, 20)) //
+                        .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(40)),
+                new Triangle(new Point3D(1500, -1500, -1500), new Point3D(-1500, 1500, -1500),
+                        new Point3D(670, 670, 3000)) //
+                        .setEmission(new Color(20, 20, 20)) //
+                        .setMaterial(new Material().setKr(1)),
+                new Triangle(new Point3D(1500, -1500, -1500), new Point3D(-1500, 1500, -1500),
+                        new Point3D(-1500, -1500, -2000)) //
+                        .setEmission(new Color(20, 20, 20)) //
+                        .setMaterial(new Material().setKr(0.5)));
+
+        scene.lights.add(new SpotLight(new Color(1020, 400, 400), new Point3D(-750, -750, -150), new Vector(-1, -1, -4)) //
+                .setKl(0.00001).setKq(0.000005));
+
+        ImageWriter imageWriter = new ImageWriter("allEffectsTest4", 500, 500);
+        Render render = new Render() //
+                .setImageWriter(imageWriter) //
+                .setCamera(camera) //
+                .setRayTracer(new BasicRayTracer(scene));
+
+        render.renderImage();
+        render.writeToImage();
+    }
+
 }
 
 
