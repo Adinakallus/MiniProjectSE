d10894cad2768ec16c1ff9b4c2b71024a355ffed
adina kallus <adina.ck.2000@gmail.com>
2021-06-03 19:38:07 +0300

    played around with our picture

--- Tests/elements/ReflectionRefractionTests.java
+++ Tests/elements/ReflectionRefractionTests.java
@@ -138,22 +138,26 @@ public class ReflectionRefractionTests {
                 new Sphere(450, new Point3D(-600, -477, -1100)) //
                         .setEmission(new Color(0, 0, 100)) //
                         .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20).setKt(0.6)),
-                new Sphere(200, new Point3D(-520, -700, -1090)) //
+                new Sphere(200, new Point3D(-500, -700, -1090)) //
                         .setEmission(new Color(100, 20, 20)) //
                         .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(40)),
+                new Triangle(new Point3D(1500, -1500, -1500), new Point3D(-1500, 1500, -1500),
+                        new Point3D(-1500, -1500, -2000)) //
+                        .setEmission(new Color(20, 20, 20)) //
+                        .setMaterial(new Material().setKr(0.5)),
                 new Triangle(new Point3D(1500, -1500, -1500), new Point3D(-1500, 1500, -1500),
                         new Point3D(670, 670, 3000)) //
                         .setEmission(new Color(20, 20, 20)) //
                         .setMaterial(new Material().setKr(1)),
-                new Triangle(new Point3D(1500, -1500, -1500), new Point3D(-1500, 1500, -1500),
-                        new Point3D(-1500, -1500, -2000)) //
-                        .setEmission(new Color(20, 20, 20)) //
-                        .setMaterial(new Material().setKr(0.5)));
+                new Triangle(new Point3D(-1200, -1000, 500), new Point3D(-700,-1000, 500),
+                new Point3D(-600, -200,2000))
+                .setEmission(new Color(20 ,1, 0))
+                .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20).setKt(0.9)));
 
         scene.lights.add(new SpotLight(new Color(1020, 400, 400), new Point3D(-750, -750, -150), new Vector(-1, -1, -4)) //
                 .setKl(0.00001).setKq(0.000005));
 
-        ImageWriter imageWriter = new ImageWriter("allEffectsTest4", 500, 500);
+        ImageWriter imageWriter = new ImageWriter("allEffectsTest8", 500, 500);
         Render render = new Render() //
                 .setImageWriter(imageWriter) //
                 .setCamera(camera) //
